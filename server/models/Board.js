0d93e3c9379b31a0d78217f7c40ee6b57f556381
Rashmi Rathnasiri <mrash7882@gmail.com>
2026-08-20 08:48:04 +0530

    feat: implement board CRUD API

---
+++ server/models/Board.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose');
+
+const BoardSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      trim: true
+    },
+
+    owner: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User',
+      required: true
+    },
+
+    members: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User'
+      }
+    ]
+  },
+  {
+    timestamps: true
+  }
+);
+
+module.exports = mongoose.model('Board', BoardSchema);
